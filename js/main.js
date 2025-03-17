dbf6768ec64b42379da32060d455c46d58f165ef
CHCAT1320 <108920403+CHCAT1320@users.noreply.github.com>
2025-03-17 21:05:05 +0800

    test

---
+++ js/main.js
@@ -0,0 +1,30 @@
+async function fetchDataAndProcess() {
+    let data;
+    try {
+      const response = await fetch('https://chcat1320.github.io/TurboWarpExtensions-cn/info.json');
+      data = await response.json();
+    } catch (error) {
+      console.error('Error:', error);
+    }
+    return data;
+  }
+  
+var jsonData
+
+fetchDataAndProcess().then(data => {
+jsonData = data;
+console.log(jsonData);
+main()
+});
+
+function main(){
+  var extensionsList = document.getElementById("extensionsList")
+  for(let i = 0;i < jsonData.extensions.length;i++){
+    //for(let i = 0;i < 10;i++){
+    var newExtension = document.createElement('div')
+    newExtension.id = "extensionsBox"
+    newExtension.innerHTML = `<img src="./svg/${jsonData.extensions[i].name}.svg" style="width: 300px;height: 150px;"/>` + jsonData.extensions[i].name
+    extensionsList.appendChild(newExtension)
+    console.log(i)
+  }
+}
